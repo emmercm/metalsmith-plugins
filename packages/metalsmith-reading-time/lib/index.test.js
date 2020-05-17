b1c5e8b84fbe325a83ddfca979f043d81a0db0f0
Christian Emmer <christianemmer@stockx.com>
2020-05-17 19:09:56 -0400

    Miscellaneous (#4)
    * Tests for different path separators.
    
    * Renovate update.

--- packages/metalsmith-reading-time/lib/index.test.js
+++ packages/metalsmith-reading-time/lib/index.test.js
@@ -13,41 +13,73 @@ const assertDir = require('assert-dir-equal');
 const readingTime = require('./index');
 
 const test = (dir, config) => {
-  it(`should build the directory "${dir}"`, (done) => {
+  describe(dir, () => {
     // Allow src directory to not exist / be empty and not committed
     if (!existsSync(`${dir}/src`)) {
       mkdirSync(`${dir}/src`);
     }
 
-    Metalsmith(`${dir}`)
-      .use(readingTime(config.options))
-      .use(hbtmd(handlebars))
-      .build((err) => {
-        if (config.error) {
-          expect(err)
-            .toBe(config.error);
-        } else {
-          expect(err)
-            .toBeNull();
-        }
+    ['/', '\\'].forEach((pathSeparator) => {
+      it(`should build with path separator "${pathSeparator}"`, (testDone) => {
+        Metalsmith(`${dir}`)
+          // Convert the path separators to the one being tested
+          .use((files, metalsmith, done) => {
+            Object.keys(files)
+              .forEach((filename) => {
+                const newFilename = filename.replace(/[/\\]/g, pathSeparator);
+                if (newFilename !== filename) {
+                  files[newFilename] = files[filename];
+                  delete files[filename];
+                }
+              });
+            done();
+          })
+          // Run the plugin
+          .use(readingTime(config.options))
+          .use(hbtmd(handlebars))
+          // Convert the path separators back to system default
+          .use((files, metalsmith, done) => {
+            const properPathSeparator = process.platform === 'win32' ? '\\' : '/';
+            Object.keys(files)
+              .forEach((filename) => {
+                const newFilename = filename.replace(/[/\\]/g, properPathSeparator);
+                if (newFilename !== filename) {
+                  files[newFilename] = files[filename];
+                  delete files[filename];
+                }
+              });
+            done();
+          })
+          // Test the output
+          .build((err) => {
+            if (config.error) {
+              expect(err)
+                .toBe(config.error);
+            } else {
+              expect(err)
+                .toBeNull();
+            }
 
-        if (err) {
-          done();
-          return;
-        }
+            if (err) {
+              testDone();
+              return;
+            }
 
-        assertDir(`${dir}/build`, `${dir}/expected`, { filter: () => true });
-        done();
+            assertDir(`${dir}/build`, `${dir}/expected`, { filter: () => true });
+            testDone();
+          });
       });
+    });
   });
 };
 
-describe('metalsmith-reading-time', () => {
+describe('metalsmith-html-unused', () => {
   const dirs = (p) => readdirSync(p)
     .map((f) => join(p, f))
     .filter((f) => statSync(f).isDirectory());
-  dirs('lib/fixtures').forEach((dir) => {
-    const config = existsSync(`${dir}/config.json`) ? JSON.parse(readFileSync(`${dir}/config.json`).toString()) : {};
-    test(dir, config);
-  });
+  dirs('lib/fixtures')
+    .forEach((dir) => {
+      const config = existsSync(`${dir}/config.json`) ? JSON.parse(readFileSync(`${dir}/config.json`).toString()) : {};
+      test(dir, config);
+    });
 });
