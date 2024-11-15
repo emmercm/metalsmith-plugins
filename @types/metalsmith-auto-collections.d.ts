89cc479c3b496518ea930d80ef43e044bf62547b
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-11-15 14:01:32 -0800

    Remove some unnecessary custom types

--- @types/metalsmith-auto-collections.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'metalsmith-auto-collections' {
-    import Metalsmith from "metalsmith";
-
-    function metalsmithAutoCollections(files: {pattern?: string, settings?: object, collections?: object}): Metalsmith.Plugin;
-
-    export = metalsmithAutoCollections;
-}
